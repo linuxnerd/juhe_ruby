1702081e0ba2cf975d0aaad520c997a2ca4db656
Eric Lou <lwc_evale@hotmail.com>
2014-05-24 23:25:51 +0800

    可读性更强

--- lib/juhe_ruby.rb
+++ lib/juhe_ruby.rb
@@ -5,7 +5,9 @@ module Juhe
   class << self
     modules = Juhe.constants.map{ |el| Juhe.const_get(el) }.select{ |m| m.instance_of?(Module) }
     modules.each do |mod|
-      attr_accessor "#{$1.downcase}_app_key".to_sym if mod.to_s =~ /\AJuhe::(.*)/
+      if mod.to_s =~ /\AJuhe::(.*)/
+        attr_accessor "#{$1.downcase}_app_key".to_sym
+      end
     end
   end
 end

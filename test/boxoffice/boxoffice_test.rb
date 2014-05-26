fe083f3c7d1b7b5e621c12e16c32c80fefe0710b
sunday35034 <lwc_evale@hotmail.com>
2014-05-26 12:15:51 +0800

    完善电影票房测试案例

--- test/boxoffice/boxoffice_test.rb
+++ test/boxoffice/boxoffice_test.rb
@@ -11,22 +11,22 @@ class Juhe::BoxofficeTest < Test::Unit::TestCase
   end
 
   def test_latest_without_app_key
-    assert_not_nil(Juhe::Boxoffice.latest("hk"))
+    assert_not_nil(Juhe::Boxoffice.latest("hk").first["rid"])
   end
 
   def test_latest_with_app_key
     Juhe::Boxoffice.app_key = ""
     result = Juhe::Boxoffice.latest("hk", app_key: "8b5f6d4adb46123938ebad000ca09300")
-    assert_not_nil(result)
+    assert_not_nil(result.first["rid"])
   end
 
   def test_wp_without_app_key
-    assert_not_nil(Juhe::Boxoffice.wp)
+    assert_not_nil(Juhe::Boxoffice.wp.first["name"])
   end
 
   def test_wp_with_app_key
     Juhe::Boxoffice.app_key = ""
     result = Juhe::Boxoffice.wp(app_key: "8b5f6d4adb46123938ebad000ca09300")
-    assert_not_nil(result)
+    assert_not_nil(result.first["name"])
   end
 end
